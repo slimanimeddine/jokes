7023ad9bf5f3ba2d8ebdf9cc2d44f338d0f45ca8
Slimani Imed Eddine Abderrahmane <imeds2000@gmail.com>
2023-06-29 15:16:35 +0100

    first commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,16 @@
+import type { V2_MetaFunction } from "@remix-run/node";
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+}
